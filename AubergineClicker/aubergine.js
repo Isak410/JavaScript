96f785f2775f90b8f0a5f70248f95479555c13c8
Isak410 <Isakstenep@gmail.com>
2023-05-10 13:52:03 +0200

    level

--- AubergineClicker/aubergine.js
+++ AubergineClicker/aubergine.js
@@ -7,19 +7,19 @@ var audio1 = new Audio('rizz.mp3');
 var clickdamage = 1
 var kjeks = 0
 
-var knapp = document.querySelector('#klikk') 
+var EL_knapp = document.querySelector('#klikk') 
 
-var knappup1 = document.querySelector('#up1') 
-var knappup2 = document.querySelector('#up2') 
-var knappup3 = document.querySelector('#up3') 
-var knappup4 = document.querySelector('#up4') 
-var knappup5 = document.querySelector('#up5')
+var EL_knappup1 = document.querySelector('#up1') 
+var EL_knappup2 = document.querySelector('#up2') 
+var EL_knappup3 = document.querySelector('#up3') 
+var EL_knappup4 = document.querySelector('#up4') 
+var EL_knappup5 = document.querySelector('#up5')
 
-var knappdps1 = document.querySelector('#dps1') 
-var knappdps2 = document.querySelector('#dps2') 
-var knappdps3 = document.querySelector('#dps3') 
-var knappdps4 = document.querySelector('#dps4') 
-var knappdps5 = document.querySelector('#dps5')
+var EL_knappdps1 = document.querySelector('#dps1') 
+var EL_knappdps2 = document.querySelector('#dps2') 
+var EL_knappdps3 = document.querySelector('#dps3') 
+var EL_knappdps4 = document.querySelector('#dps4') 
+var EL_knappdps5 = document.querySelector('#dps5')
 
 var restartknapp = document.querySelector('#ascend') 
 //var svar = document.querySelector('#svar')   
@@ -36,11 +36,11 @@ var up3v = 0
 var up4v = 0
 var up5v = 0
 
-var dpslv1 = 0
-var dpslv2 = 0
-var dpslv3 = 0
-var dpslv4 = 0
-var dpslv5 = 0
+var vdpslv1 = 0
+var vdpslv2 = 0
+var vdpslv3 = 0
+var vdpslv4 = 0
+var vdpslv5 = 0
 
 var scorea = 0
 
@@ -80,9 +80,7 @@ var up4cost = 15000
 var up5cost = 60000
 
 
-
 function upgrade1() {
-        
         if (kjeks>up1cost) {
             up1k = up1k+1
             up1v = up1v+1
@@ -178,12 +176,9 @@ function upgrade5() {
             if (kjeks>up1cost) {
                 kjeks = kjeks-up1cost
                 dps = dps + 0.1
-                dpslv1 = dpslv1 + 1
-                console.log (dpslv1)
-                dpslv1.innerHTML = "Lvl. "+dpslv1
-                
+                vdpslv1 = vdpslv1 + 1
+                dpslv1.innerHTML = "Lvl. "+vdpslv1
                 update_HTML()
-                
             }
         }
 
@@ -191,6 +186,8 @@ function upgrade5() {
             if (kjeks>up2cost) {
                 kjeks = kjeks-up2cost
                 dps = dps + 0.5
+                vdpslv2 = vdpslv2 + 1
+                dpslv2.innerHTML = "Lvl. "+vdpslv2
                 update_HTML()
             }
         }
@@ -199,6 +196,8 @@ function upgrade5() {
             if (kjeks>up3cost) {
                 kjeks = kjeks-up3cost
                 dps = dps + 2.0
+                vdpslv3 = vdpslv3 + 1
+                dpslv3.innerHTML = "Lvl. "+vdpslv3
                 update_HTML()
             }
         }
@@ -207,6 +206,8 @@ function upgrade5() {
             if (kjeks>up4cost) {
                 kjeks = kjeks-up4cost
                 dps = dps + 10.0
+                vdpslv4 = vdpslv4 + 1
+                dpslv4.innerHTML = "Lvl. "+vdpslv4
                 update_HTML()
             }
         }
@@ -215,6 +216,8 @@ function upgrade5() {
             if (kjeks>up5cost) {
                 kjeks = kjeks-up5cost
                 dps = dps + 50.0
+                vdpslv5 = vdpslv5 + 1
+                dpslv5.innerHTML = "Lvl. "+vdpslv5
                 update_HTML()
             }
         }
@@ -238,18 +241,18 @@ function upgrade5() {
         update_HTML()
     }  
 
-    knapp.addEventListener('click', sjekk)
-    knappup1.addEventListener('click', upgrade1)
-    knappup2.addEventListener('click', upgrade2)
-    knappup3.addEventListener('click', upgrade3)
-    knappup4.addEventListener('click', upgrade4)
-    knappup5.addEventListener('click', upgrade5)
+    EL_knapp.addEventListener('click', sjekk)
+    EL_knappup1.addEventListener('click', upgrade1)
+    EL_knappup2.addEventListener('click', upgrade2)
+    EL_knappup3.addEventListener('click', upgrade3)
+    EL_knappup4.addEventListener('click', upgrade4)
+    EL_knappup5.addEventListener('click', upgrade5)
 
-    knappdps1.addEventListener('click', dps1)
-    knappdps2.addEventListener('click', dps2)
-    knappdps3.addEventListener('click', dps3)
-    knappdps4.addEventListener('click', dps4)
-    knappdps5.addEventListener('click', dps5)
+    EL_knappdps1.addEventListener('click', dps1)
+    EL_knappdps2.addEventListener('click', dps2)
+    EL_knappdps3.addEventListener('click', dps3)
+    EL_knappdps4.addEventListener('click', dps4)
+    EL_knappdps5.addEventListener('click', dps5)
     //restartknapp.addEventListener('click', ascend)
     
     intervalID = setInterval(myTimer, 1000) //kjører hvert 1000ms (1sek = 1000ms)
