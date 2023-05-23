fd7894759f7da434c73c0d4e77478d4a4d95dba2
Isak410 <Isakstenep@gmail.com>
2023-05-23 15:17:15 +0200

    dasdgasgdad

--- AubergineClicker/aubergine.js
+++ AubergineClicker/aubergine.js
@@ -1,6 +1,10 @@
 var goldonscreen = false
 var goldonscreennumber = 0
 
+var boost1timer = 0
+
+var boost1active = false
+
 var randtall = 0
 var totalcookies = 0
 var dps = 0
@@ -30,6 +34,12 @@ var EL_knappdps4 = document.querySelector('#dps4')
 var EL_knappdps5 = document.querySelector('#dps5')
 var EL_dps13 = document.querySelector('#dps13')
 
+var EL_knappboost1 = document.querySelector('#boost1') 
+var EL_knappboost2 = document.querySelector('#boost2') 
+var EL_knappboost3 = document.querySelector('#boost3') 
+var EL_knappboost4 = document.querySelector('#boost4') 
+var EL_knappboost5 = document.querySelector('#boost5')
+
 var EL_knappsave = document.querySelector('#save')
 var EL_knappload = document.querySelector('#load')
 var EL_autosave = document.querySelector('#autosave')
@@ -56,6 +66,8 @@ var vdpslv4 = 0
 var vdpslv5 = 0
 
 var scorea = 0
+
+var realdpc = clickdamage
 
 function ascend() {
     scorea = 
@@ -239,12 +251,30 @@ function upgrade5() {
                 document.querySelector('#dpsup5d').innerHTML = "Not enough Aubergines"
                 }  
         }
+        
+        
+        function boost1() {
+            //console.log("goober")
+            tdpc.innerHTML = "Total Dpc: "+((up1k+up2k+up3k+up4k+up5k+1)*10)
+            boost1timer = 10
+            boost1active = true
+        }
+
 
 
 
     function sjekk() {
-        kjeks = kjeks + clickdamage 
-        totalcookies = totalcookies + clickdamage
+        
+        if (boost1active == true) {
+            console.log("boost1active true fjlahflk")
+            realdpc = clickdamage * 10
+            kjeks = kjeks + realdpc
+            befor.innerHTML = "Aubergines: "+kjeks
+            console.log(realdpc+"sdad")
+        } else {
+            kjeks = kjeks + clickdamage
+        }
+        
         kjeksr = Math.round(kjeks)
         befor.innerHTML = "Aubergines: "+kjeksr
         up1d.innerHTML = ""
@@ -252,12 +282,15 @@ function upgrade5() {
         up3d.innerHTML = ""
         up4d.innerHTML = ""
         up5d.innerHTML = ""
+
+        //clear "not enough aubergines" felt
         document.querySelector('#dpsup1d').innerHTML = ""
         document.querySelector('#dpsup2d').innerHTML = ""
         document.querySelector('#dpsup3d').innerHTML = ""
         document.querySelector('#dpsup4d').innerHTML = ""
         document.querySelector('#dpsup5d').innerHTML = ""
-        
+
+        totalcookies = totalcookies + clickdamage
         randtall = Math.floor(Math.random() * 1000);
         console.log(randtall)
         if (randtall == 500) {
@@ -277,15 +310,30 @@ function upgrade5() {
             save()
             autosavetime = 0
         }
-        if (goldonscreen = true) {
+        if (goldonscreen == true) {
             goldonscreennumber = goldonscreennumber + 1
             if (goldonscreennumber > 3) {
                 document.getElementById("goldaubergine").style.display = "none";
                 goldonscreennumber = 0
                 goldonscreen = false
             }
+        };
+        if (boost1active == true) {
+            new Audio('rizz.mp3').play();
+            tdpc.innerHTML = "Total Dpc: "+((up1k+up2k+up3k+up4k+up5k+1)*10)
+            //console.log("boost1active true fjlahflk")
+            
+            realdpc = clickdamage * 10
+            console.log(realdpc+"sdad")
+            boost1timer = boost1timer - 1
+            console.log(boost1timer)
+            document.getElementById('boostno1').innerHTML = boost1timer
+            if (boost1timer == 1) {
+                tdpc.innerHTML = "Total Dpc: "+(up1k+up2k+up3k+up4k+up5k+1)
+                boost1active = false
+                }
+            }  
         }
-    }  
     
     function save() {
         localStorage.kjeksST = kjeks
@@ -401,6 +449,12 @@ function upgrade5() {
     EL_knappdps4.addEventListener('click', dps4)
     EL_knappdps5.addEventListener('click', dps5)
     //restartknapp.addEventListener('click', ascend)
+
+    EL_knappboost1.addEventListener('click',boost1)
+    EL_knappboost2.addEventListener('click',boost2)
+    EL_knappboost3.addEventListener('click',boost3)
+    EL_knappboost4.addEventListener('click',boost4)
+    EL_knappboost5.addEventListener('click',boost5)
 
     EL_knappsave.addEventListener('click', save)
     EL_knappload.addEventListener('click', load)
