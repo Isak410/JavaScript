086acddb066d9474c645b2a2436501a3f8f54e91
Isak410 <113894543+Isak410@users.noreply.github.com>
2023-06-01 18:38:15 +0200

    masse shit

---
+++ Boom calculator/script.js
@@ -0,0 +1,53 @@
+const input1 = document.querySelector('#beancanin')
+const input2 = document.querySelector('#satchelin')
+const input3 = document.querySelector('#exploin')
+const input4 = document.querySelector('#rocketin')
+const input5 = document.querySelector('#c4in')
+var test1 = 0
+
+const EL_submitknapp = document.querySelector('#submitknappdiv')
+
+
+
+
+    function beancan1func() {
+        
+
+    }
+
+    function satchel1func() {
+
+        
+    }
+
+    function explo1func() {
+
+        
+    }
+
+    function rocket1func() {
+
+        
+    }
+
+    function c41func() {
+
+        
+    }
+
+
+    function submit4() {
+        console.log("neger")
+    }
+
+
+
+    input1.addEventListener('click', beancan1func)
+    input2.addEventListener('click', satchel1func)
+    input3.addEventListener('click', explo1func)
+    input4.addEventListener('click', rocket1func)
+    input5.addEventListener('click', c41func)
+
+    EL_submitknapp.addEventListener('click', submit4)
+
+
