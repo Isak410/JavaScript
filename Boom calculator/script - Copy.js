24e6af975aa3c94315915ed3f2264965fc288d62
Isak410 <Isakstenep@gmail.com>
2023-06-02 13:33:15 +0200

    yuh

---
+++ Boom calculator/script - Copy.js
@@ -0,0 +1,92 @@
+const input1 = document.querySelector('#beancanin')
+const input2 = document.querySelector('#satchelin')
+const input3 = document.querySelector('#exploin')
+const input4 = document.querySelector('#rocketin')
+const input5 = document.querySelector('#c4in')
+
+const EL_submitknapp = document.querySelector('#submitknapp')
+
+var value1 = 0
+var value2 = 0
+var value3 = 0
+var value4 = 0
+var value5 = 0
+
+var beancantotal = 0
+var satcheltotal = 0
+var explototal = 0
+var rockettotal = 0
+var c4total = 0
+
+var beancan1total = 0
+var satchel1total = 0
+var explo1total = 0
+var rocket1total = 0
+var c41total = 0
+
+var beancan2total = 0
+var satchel2total = 0
+var explo2total = 0
+var rocket2total = 0
+var c42total = 0
+
+var beancan3total = 0
+var satchel3total = 0
+var explo3total = 0
+var rocket3total = 0
+var c43total = 0
+
+var totalsulfur = 0
+var totalgp = 0
+var totalmetal = 0
+var totallowgrade = 0
+
+    function submit4() {
+        //hvor mange av hvert item
+        value1 = document.getElementById('beancanin').value
+        value2 = document.getElementById('satchelin').value
+        value3 = document.getElementById('exploin').value
+        value4 = document.getElementById('rocketin').value
+        value5 = document.getElementById('c4in').value
+
+        //sulfur
+        beancan3total = (value1*120)
+        satchel3total = (value2*480)
+        explo3total = (value3*14.5)
+        rocket3total = (value4*1100)
+        c43total =  (value5*2200)
+
+
+        //gp
+        beancantotal = (value1*60)
+        satcheltotal = (value2*240)
+        explototal = (value3*12.5)
+        rockettotal = (value4*700)
+        c4total = (value5*1100)
+
+        //metal
+        beancan1total = (value1*20)
+        satchel1total = (value2*80)
+        explo1total = (value3*5)
+        rocket1total = (value4*100)
+        c41total = (value5*200)
+
+        //lowgrade
+        beancan2total = (value1*0)
+        satchel2total = (value2*0)
+        explo2total = (value3*0)
+        rocket2total = (value4*30)
+        c42total = (value5*60)
+
+        //innerhtml av hver item type
+        totalsulfur = (beancan3total + satchel3total + explo3total + rocket3total + c43total)
+        totalgp = (beancantotal + satcheltotal + explototal + rockettotal + c4total)
+        totalmetal = (beancan1total + satchel1total + explo1total + rocket1total + c41total)
+        totallowgrade = (beancan2total + satchel2total + explo2total + rocket2total + c42total)
+        document.getElementById('totalsulfur').innerHTML = ("(You need a total of: " +totalsulfur+ " Sulfur)")
+        document.getElementById('totalgp').innerHTML = ("You need a total of: " +totalgp+ " Gunpowder")
+        document.getElementById('totalmetal').innerHTML = ("You need a total of: " +totalmetal+ " Metal")
+        document.getElementById('totallowgrade').innerHTML = ("You need a total of: " +totallowgrade+ " Lowgrade")
+    }
+
+    EL_submitknapp.addEventListener('click', submit4)
