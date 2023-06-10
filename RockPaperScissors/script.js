a99f226356cd874a06c7077ee3935635f523d207
Isak410 <Isakstenep@gmail.com>
2023-06-10 15:11:23 +0200

    RPC ferdig

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -1,20 +1,30 @@
+//knappen man trykker på for å starte
 const EL_begin = document.querySelector('#begin')
 
+//Bilde på venstre og høyre side
 const EL_imgL = document.querySelector('#IMGL')
 const EL_imgR = document.querySelector('#IMGR')
+
+//p-taggen man får vite hvem som vinner i
 const EL_svar = document.querySelector('#svar')
 
+//hva som blir velgt av hver side
 var handL = 0
 var handR = 0
 
+//med å bestemme hvem som vinner
+var handMINUSL = 0
+var handMINUSR = 0
 
-
-
-
+//Function som skal runne når man trykker på knappen
 function RPC() {
+    //Define hvem som velger hva
     handL = Math.floor(Math.random() * 3) + 1;
     handR = Math.floor(Math.random() * 3) + 1;
-    
+    handMINUSL = (handL - handR)
+    handMINUSR = (handR - handL)
+
+    //Assign nytt bilde på Venstre side
     if (handL == 1) {
         EL_imgL.src = "./Assets/IMG/Rock.png"
     }   else if (handL == 2) {
@@ -25,6 +35,7 @@ function RPC() {
         console.log("L if funker ikke")
     }
 
+    //Assign nytt bilde på høyre side
     if (handR == 1) {
         EL_imgR.src = "./Assets/IMG/Rock.png"
     }   else if (handR == 2) {
@@ -35,33 +46,25 @@ function RPC() {
         console.log("R if funker ikke")
     }
 
-    console.log(handL-handR)
-    
+
+    //Calculate hvem som vinner
     if (handL > handR) {
-        if (!handL-handR == -2) {
-            EL_svar.innerHTML = ("Venstre vant")
-        }
+        EL_svar.innerHTML = ("Venstre vant")
     }
     if (handR > handL) {
-        if (!handR-handL == -2) {
-            EL_svar.innerHTML = ("Høyre vant")
-        }      
+        EL_svar.innerHTML = ("Høyre vant")      
     }
-    if (handL - )
-
+    if (handMINUSR == 2) {
+        EL_svar.innerHTML = ("Venstre vant")
+    }
+    if (handMINUSL == 2) {
+        EL_svar.innerHTML = ("Høyre vant")
+    }
     if (handL-handR == 0) {
         EL_svar.innerHTML = ("Gjevnt")
+    }
+
 }
-}
 
-
-
-
-//0 = draw
-//1 = 
-
-
-
-
-
+//hva som skal skje når man trykker på knappen
 EL_begin.addEventListener('click', RPC)
