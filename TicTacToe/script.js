72af8442e1695f8e163c4401b8baadb9d592aa1b
Isak410 <113894543+Isak410@users.noreply.github.com>
2023-06-19 19:21:27 +0200

    Tic Tac Toe

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -10,6 +10,8 @@ const Slot7 = document.querySelector('#slot7')
 const Slot8 = document.querySelector('#slot8')
 const Slot9 = document.querySelector('#slot9')
 
+const svar = document.querySelector('#svar')
+
 const tur = document.querySelector('#tur')
 
 var testbool = false
@@ -26,14 +28,68 @@ var pressed7 = false
 var pressed8 = false
 var pressed9 = false
 
+var slot1value = 0
+var slot2value = 0
+var slot3value = 0
+
+var slot4value = 0
+var slot5value = 0
+var slot6value = 0
+
+var slot7value = 0
+var slot8value = 0
+var slot9value = 0
+
+const xvinner = 297
+const ovinner = 300
+
+
+function sjekkvinner() {
+    if (slot1value+slot2value+slot3value == xvinner) {
+        svar.innerHTML = "X vant"
+    } else if (slot4value+slot5value+slot6value == xvinner) {
+        svar.innerHTML = "X vant"
+    } else if (slot7value+slot8value+slot9value == xvinner) {
+        svar.innerHTML = "X vant"
+    } else if (slot1value+slot4value+slot7value == xvinner) {
+        svar.innerHTML = "X vant"
+    } else if (slot2value+slot5value+slot8value == xvinner) {
+        svar.innerHTML = "X vant"
+    } else if (slot3value+slot6value+slot9value == xvinner) {
+        svar.innerHTML = "X vant"
+    } else if (slot1value+slot5value+slot9value == xvinner) {
+        svar.innerHTML = "X vant"
+    } else if (slot3value+slot5value+slot7value == xvinner) {
+        svar.innerHTML = "X vant"
+    } else if (slot1value+slot2value+slot3value == ovinner) {
+        svar.innerHTML = "O vant"
+    } else if (slot4value+slot5value+slot6value == ovinner) {
+        svar.innerHTML = "O vant"
+    } else if (slot7value+slot8value+slot9value == ovinner) {
+        svar.innerHTML = "O vant"
+    } else if (slot1value+slot4value+slot7value == ovinner) {
+        svar.innerHTML = "O vant"
+    } else if (slot2value+slot5value+slot8value == ovinner) {
+        svar.innerHTML = "O vant"
+    } else if (slot3value+slot6value+slot9value == ovinner) {
+        svar.innerHTML = "O vant"
+    } else if (slot1value+slot5value+slot9value == ovinner) {
+        svar.innerHTML = "O vant"
+    } else if (slot3value+slot5value+slot7value == ovinner) {
+        svar.innerHTML = "O vant"
+    }
+}
+
 
 function sjekktur() {
     if (testbool == false) {
         testbool = true
         turn = "x"
+        turtekst = "O"
     }   else {
         testbool = false
         turn = "o"
+        turtekst = "X"
     }
 }
 
@@ -44,6 +100,7 @@ function turtekst() {
         turtekst = "X"
     }
     tur.innerHTML = turtekst
+    sjekkvinner()
 }
 
 
@@ -51,81 +108,135 @@ function trykk1() {
 
     if (pressed1 == false) {
         sjekktur()
+        if (turn == "x") {
+            slot1value = 99
+        }   else {
+            slot1value = 100
+        }
         Slot1.style.backgroundImage = 'url("./Assets/IMG/'+ turn + '.png")'
         pressed1 = true
-        turtekst()
+        tur.innerHTML = turtekst
+        sjekkvinner()
     }
 }
 
 function trykk2() {
     if (pressed2 == false) {
         sjekktur()
+        if (turn == "x") {
+            slot2value = 99
+        }   else {
+            slot2value = 100
+        }
         Slot2.style.backgroundImage = 'url("./Assets/IMG/'+ turn + '.png")'
         pressed2 = true
-        turtekst()
+        tur.innerHTML = turtekst
+        sjekkvinner()
     }
 }
 
 function trykk3() {
     if (pressed3 == false) {
         sjekktur()
+        if (turn == "x") {
+            slot3value = 99
+        }   else {
+            slot3value = 100
+        }
         Slot3.style.backgroundImage = 'url("./Assets/IMG/'+ turn + '.png")'
         pressed3 = true
-        turtekst()
+        tur.innerHTML = turtekst
+        sjekkvinner()
     }
 }
 
 function trykk4() {
     if (pressed4 == false) {
         sjekktur()
+        if (turn == "x") {
+            slot4value = 99
+        }   else {
+            slot4value = 100
+        }
         Slot4.style.backgroundImage = 'url("./Assets/IMG/'+ turn + '.png")'
         pressed4 = true
-        turtekst()
+        tur.innerHTML = turtekst
+        sjekkvinner()
     }
 }
 
 function trykk5() {
     if (pressed5 == false) {
         sjekktur()
+        if (turn == "x") {
+            slot5value = 99
+        }   else {
+            slot5value = 100
+        }
         Slot5.style.backgroundImage = 'url("./Assets/IMG/'+ turn + '.png")'
         pressed5 = true
-        turtekst()
+        tur.innerHTML = turtekst
+        sjekkvinner()
     }
 }
 
 function trykk6() {
     if (pressed6 == false) {
         sjekktur()
+        if (turn == "x") {
+            slot6value = 99
+        }   else {
+            slot6value = 100
+        }
         Slot6.style.backgroundImage = 'url("./Assets/IMG/'+ turn + '.png")'
         pressed6 = true
-        turtekst()
+        tur.innerHTML = turtekst
+        sjekkvinner()
     }
 }
 
 function trykk7() {
     if (pressed7 == false) {
         sjekktur()
+        if (turn == "x") {
+            slot7value = 99
+        }   else {
+            slot7value = 100
+        }
         Slot7.style.backgroundImage = 'url("./Assets/IMG/'+ turn + '.png")'
         pressed7 = true
-        turtekst()
+        tur.innerHTML = turtekst
+        sjekkvinner()
     }
 }
 
 function trykk8() {
     if (pressed8 == false) {
         sjekktur()
+        if (turn == "x") {
+            slot8value = 99
+        }   else {
+            slot8value = 100
+        }
         Slot8.style.backgroundImage = 'url("./Assets/IMG/'+ turn + '.png")'
         pressed8 = true
-        turtekst()
+        tur.innerHTML = turtekst
+        sjekkvinner()
     }
 }
 
 function trykk9() {
     if (pressed9 == false) {
         sjekktur()
+        if (turn == "x") {
+            slot9value = 99
+        }   else {
+            slot9value = 100
+        }
         Slot9.style.backgroundImage = 'url("./Assets/IMG/'+ turn + '.png")'
         pressed9 = true
-        turtekst()
+        tur.innerHTML = turtekst
+        sjekkvinner()
     }
 }
 
